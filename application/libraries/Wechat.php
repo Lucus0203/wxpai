036a56644f89bf86734327a6b6bae06c5beb5180
Lucus0203 <liting68@163.com>
2016-07-28 14:48:30 +0800

    Wechat

--- application/libraries/Wechat.php
+++ application/libraries/Wechat.php
@@ -42,7 +42,7 @@ class Wechat
 
     function setJsticket()
     {
-        $ticketurl = "hhttps://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=" . $this->_access_token;
+        $ticketurl = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=" . $this->_access_token;
         $data = $this->returnWeChatJsonData($ticketurl);
         $this->_jsapi_ticket = $data->ticket;
         file_put_contents($this->_jsapi_ticket_file, $data->ticket);
