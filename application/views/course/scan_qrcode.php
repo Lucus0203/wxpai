3678b8f69b03776fb885417fcd2b5cdde2db0b5b
Lucus0203 <liting68@163.com>
2016-07-28 16:47:24 +0800

    微信扫码

--- application/views/course/scan_qrcode.php
+++ application/views/course/scan_qrcode.php
@@ -15,8 +15,8 @@
                     <a href="javascript:void(0)" class="grayH42 fLeft">签到未开始</a>
                 <?php }elseif($course['signin_end']<date("Y-m-d H:i:s")){ ?>
                     <a href="javascript:void(0)" class="grayH42 fLeft">签到已结束</a>
-                <?php }else{ ?>
-                    <a href="javascript:void(0)" id="siginInBtn" class="grayH42 bgGreen fLeft">我要签到</a>
+                <?php }else{ $siginInBtnflag=1; ?>
+                    <a href="javascript:void(0)" class="grayH42 bgOrange fLeft">待签到</a>
                 <?php }} ?>
         </div>
         <div class="listText">
@@ -45,10 +45,14 @@
 
 
 </div>
+
+<?php if(!empty($siginInBtnflag)){ ?>
+    <div class="bottomFix"><a href="javascript:void(0)" id="siginInBtn" class="blueBtnH40" >我要签到</a></div>
+<?php } ?>
 <script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script type="text/javascript">
     wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: false,
         appId: '<?php echo $signPackage["appId"];?>',
         timestamp: <?php echo $signPackage["timestamp"];?>,
         nonceStr: '<?php echo $signPackage["nonceStr"];?>',
@@ -56,19 +60,18 @@
         jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
     wx.ready(function(){
-        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
         $('#siginInBtn').click(function(){
             wx.scanQRCode({
                 needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
                 scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
                 success: function (res) {
-                    var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-                    alert(result);
                 }
             });
         });
     });
     wx.error(function(res){
-        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        for(var i in res){
+            alert(i);
+        }
     });
 </script>
