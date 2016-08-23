d14a2c71f50a94faf66b287bad68cefbbd2c00a7
Lucus0203 <liting68@163.com>
2016-08-23 14:29:56 +0800

    update validate

--- application/controllers/Center.php
+++ application/controllers/Center.php
@@ -19,46 +19,66 @@ class Center extends CI_Controller {
 	
 	
 	public function info() {
-                $logininfo = $this->_logininfo;
-		$act = $this->input->post('act');
-		if(!empty($act)){
-                    $info=array('name'=>$this->input->post('name'),
-                        'job_code'=>$this->input->post('job_code'),
-                        'job_name'=>$this->input->post('job_name'),
-                        'department_id'=>$this->input->post('department_id'),
-                        'mobile'=>$this->input->post('mobile'),
-                        'email'=>$this->input->post('email'));
-                    $this->student_model->update($info,$logininfo['id']);
-                    $msg='资料更新成功';
-                }
-                $studentinfo=$this->student_model->get_row(array('id'=>$logininfo['id']));
-                $departments=$this->department_model->get_all(array('company_code'=>$logininfo['company_code']));
-                $this->session->set_userdata('loginInfo',$studentinfo);
-		$this->load->vars(array('loginInfo'=>$studentinfo));
-		$this->load->view ( 'header' );
-		$this->load->view ( 'center/studentinfo',array('departments'=>$departments,'msg'=>$msg) );
-		$this->load->view ( 'footer' );
+        $logininfo = $this->_logininfo;
+        $act = $this->input->post('act');
+        if(!empty($act)){
+            $info=array('name'=>$this->input->post('name'),
+                'job_code'=>$this->input->post('job_code'),
+                'job_name'=>$this->input->post('job_name'),
+                'department_parent_id'=>$this->input->post('department_parent_id'),
+                'department_id'=>$this->input->post('department_id'),
+                'mobile'=>$this->input->post('mobile'),
+                'email'=>$this->input->post('email'));
+            //验证账号
+            if ($this->student_model->get_count(" company_code = '{$logininfo['company_code']}' and mobile='".$info['mobile']."' and id <> {$logininfo['id']} and isdel = 2 ") > 0) {
+                $msg = '手机号已被他人使用';
+            } else {
+                $info['user_name']=$info['mobile'];
+                $this->student_model->update($info,$logininfo['id']);
+                $msg='资料更新成功';
+            }
+
+        }
+        $studentinfo=$this->student_model->get_row(array('id'=>$logininfo['id']));
+        $this->session->set_userdata('loginInfo',$studentinfo);
+        $this->load->vars(array('loginInfo'=>$studentinfo));
+
+        $where = "parent_id is null and company_code = '{$logininfo['company_code']}' ";
+        $departments = $this->department_model->get_all($where);
+        $second_departments = !empty($studentinfo['department_parent_id'])?$this->department_model->get_all(array('parent_id' => $studentinfo['department_parent_id'],'company_code'=>$logininfo['company_code'])):array();
+
+        $this->load->view ( 'header' );
+        $this->load->view ( 'center/studentinfo',compact('departments','second_departments','msg') );
+        $this->load->view ( 'footer' );
 	}
         
-        public function changepass(){
-                $logininfo = $this->_logininfo;
-		$act = $this->input->post('act');
-		if(!empty($act)){
-                    $cur_pass=$this->input->post('old_pass');
-                    $new_pass=$this->input->post('new_pass');
-                    if(md5($cur_pass)!=$logininfo['user_pass']){
-                        $msg='原密码错误';
-                    }else{
-                        $this->student_model->update(array('user_pass'=>md5($new_pass)),$logininfo['id']);
-                        $msg='密码更新成功';
-                    }
-                }
-                $studentinfo=$this->student_model->get_row(array('id'=>$logininfo['id']));
-                $this->session->set_userdata('loginInfo',$studentinfo);
-		$this->load->vars(array('loginInfo'=>$studentinfo));
-		$this->load->view ( 'header' );
-		$this->load->view ( 'center/changepass',array('msg'=>$msg) );
-		$this->load->view ( 'footer' );
+    public function changepass(){
+        $logininfo = $this->_logininfo;
+        $act = $this->input->post('act');
+        if(!empty($act)){
+            $cur_pass=$this->input->post('old_pass');
+            $new_pass=$this->input->post('new_pass');
+            if(md5($cur_pass)!=$logininfo['user_pass']){
+                $msg='原密码错误';
+            }else{
+                $this->student_model->update(array('user_pass'=>md5($new_pass)),$logininfo['id']);
+                $msg='密码更新成功';
+            }
         }
+        $studentinfo=$this->student_model->get_row(array('id'=>$logininfo['id']));
+        $this->session->set_userdata('loginInfo',$studentinfo);
+        $this->load->vars(array('loginInfo'=>$studentinfo));
+        $this->load->view ( 'header' );
+        $this->load->view ( 'center/changepass',array('msg'=>$msg) );
+        $this->load->view ( 'footer' );
+    }
+
+    //Ajax获取二级部门
+    public function ajaxDepartment()
+    {
+        $departmentid = $this->input->post('departmentid');
+        $departs = $this->department_model->get_all(array('parent_id' => $departmentid));
+        echo json_encode(array('departs' => $departs));
+    }
 	
 }
--- application/views/center/studentinfo.php
+++ application/views/center/studentinfo.php
@@ -1,44 +1,86 @@
 <script type="text/javascript" src="<?php echo base_url();?>js/jquery.validate.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function(){
-            $( "#infoForm" ).validate( {
-                    rules: {
-                            old_pass: {
-                                    required: true
-                            },
-                            new_pass: {
-                                    required: true,
-                                    minlength: 5
-                            },
-                            confirm_pass: {
-                                    required: true,
-                                    equalTo: "#new_pass"
-                            }
-                    },
-                    messages: {
-                            old_pass: {
-                                    required: "请输入原密码"
-                            },
-                            new_pass: {
-                                    required: "请输入新密码",
-                                    minlength: "密码长度不能小于 5 个字符"
-                            },
-                            confirm_pass: {
-                                    required: "请再输入一次新密码",
-                                    equalTo: "两次密码输入不一致"
-                            }
-                    },
-                    errorPlacement: function ( error, element ) {
-                            error.addClass( "ui red pointing label transition" );
-                            error.insertAfter( element.parent() );
-                    },
-                    highlight: function ( element, errorClass, validClass ) {
-                            $( element ).parents( ".row" ).addClass( errorClass );
-                    },
-                    unhighlight: function (element, errorClass, validClass) {
-                            $( element ).parents( ".row" ).removeClass( errorClass );
+        $('select[name=department_parent_id]').change(function(){
+            var departmentid=$(this).val();
+            $.ajax({
+                type:"post",
+                url:'<?php echo site_url('center/ajaxDepartment') ?>',
+                data:{'departmentid':departmentid},
+                datatype:'jsonp',
+                success:function(res){
+                    var json_obj = $.parseJSON(res);
+                    var count=0;
+                    var str='<option value="'+departmentid+'">请选择</option>';
+                    $.each(json_obj.departs,function(i,item){
+                        str+='<option value="'+item.id+'">'+item.name+'</option>';
+                        ++count;
+                    });
+                    if(count>0){
+                        $('select[name=department_id]').show().html(str)
+                    }else{
+                        $('select[name=department_id]').hide().html('<option value="'+departmentid+'" selected >请选择</option>');
                     }
+                }
             });
+        });
+        jQuery.validator.addMethod("isMobile", function(value, element) {
+            var length = value.length;
+            var mobile = /^((1[0-9]{2})+\d{8})$/;
+            return this.optional(element) || (length == 11 && mobile.test(value));
+        }, "请正确填写您的手机号码");
+        $( "#infoForm" ).validate( {
+            rules: {
+                name: {
+                    required: true
+                },
+                job_code: {
+                    required: true
+                },
+                job_name: {
+                    required: true
+                },
+                department_parent_id: {
+                    required: true
+                },
+                mobile: {
+                    required: true
+                },
+                email: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "请输入您的姓名"
+                },
+                job_code: {
+                    required: "请输入您的工号"
+                },
+                job_name: {
+                    required: "请输入您的职业名称"
+                },
+                department_parent_id: {
+                    required: "请选择您所在的部门"
+                },
+                mobile: {
+                    required: "请输入您的手机号码"
+                },
+                email: {
+                    required: "请输入您的点子邮箱"
+                }
+            },
+            errorPlacement: function ( error, element ) {
+                error.addClass( "ui red pointing label transition" );
+                error.insertAfter( element.parent() );
+            },
+            highlight: function ( element, errorClass, validClass ) {
+                $( element ).parents( ".row" ).addClass( errorClass );
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                $( element ).parents( ".row" ).removeClass( errorClass );
+            }
+        });
     });
 </script>
 <!--head-->
@@ -51,41 +93,48 @@
     <form id="infoForm" method="post" action="">
         <input type="hidden" name="act" value="act" />
         <dl class="dataDl">
-                <dt>您的姓名</dt>
-                <dd>
-                    <div class="noboriptBox"><input name="name" type="text" class="noboript" value="<?php echo $loginInfo['name'] ?>" >
-                        </div>
-                </dd>
-                <dt>您的工号</dt>
-                <dd>
-                    <div class="noboriptBox"><input name="job_code" type="text" class="noboript" value="<?php echo $loginInfo['job_code'] ?>">
-                        </div>
-                </dd>
-                <dt>职业名称</dt>
-                <dd>
-                    <div class="noboriptBox"><input name="job_name" type="text" class="noboript" value="<?php echo $loginInfo['job_name'] ?>">
-                        </div>
-                </dd>
-                <dt>所在部门</dt>
+                <dt><label for="name">您的姓名</label></dt>
                 <dd>
                     <div class="noboriptBox">
-                        <select name="department_id" class="noboript">
+                        <input id="name" name="name" type="text" class="noboript" value="<?php echo $loginInfo['name'] ?>" >
+                    </div>
+                </dd>
+                <dt><label for="job_code">您的工号</label></dt>
+                <dd>
+                    <div class="noboriptBox"><input id="job_code" name="job_code" type="text" class="noboript" value="<?php echo $loginInfo['job_code'] ?>">
+                        </div>
+                </dd>
+                <dt><label for="job_name">职业名称</label></dt>
+                <dd>
+                    <div class="noboriptBox"><input id="job_name" name="job_name" type="text" class="noboript" value="<?php echo $loginInfo['job_name'] ?>">
+                        </div>
+                </dd>
+                <dt><label for="department_parent_id">所在部门</label></dt>
+                <dd>
+                    <div class="noboriptBox">
+                        <select id="department_parent_id" name="department_parent_id" class="noboript">
                             <option value="">请选择</option>
                             <?php foreach ($departments as $d){ ?>
-                            <option <?php if($loginInfo['department_id']==$d['id']){ ?>selected=""<?php } ?> value="<?php echo $d['id'] ?>"><?php echo $d['level']>0?'&nbsp&nbsp':'' ?><?php echo $d['name'] ?></option>
+                            <option <?php if($loginInfo['department_parent_id']==$d['id']){ ?>selected=""<?php } ?> value="<?php echo $d['id'] ?>"><?php echo $d['name'] ?></option>
+                            <?php } ?>
+                        </select>
+                        <select id="department_id" name="department_id" <?php if(count($second_departments)<=0){?>style="display: none;"<?php } ?> class="noboript">
+                            <option value="">请选择</option>
+                            <?php foreach ($second_departments as $d){ ?>
+                                <option <?php if($loginInfo['department_id']==$d['id']){ ?>selected=""<?php } ?> value="<?php echo $d['id'] ?>"><?php echo $d['name'] ?></option>
                             <?php } ?>
                         </select>
                         </div>
 
                 </dd>
-                <dt>手机号码</dt>
+                <dt><label for="mobile">手机号码</label></dt>
                 <dd>
-                    <div class="noboriptBox"><input name="mobile" class="noboript" type="text" value="<?php echo $loginInfo['mobile'] ?>">
+                    <div class="noboriptBox"><input id="mobile" name="mobile" class="noboript" type="text" value="<?php echo $loginInfo['mobile'] ?>">
                         </div>
                 </dd>
-                <dt>电子邮箱</dt>
+                <dt><label for="email">电子邮箱</label></dt>
                 <dd>
-                    <div class="noboriptBox"><input name="email" class="noboript" type="text" value="<?php echo $loginInfo['email'] ?>">
+                    <div class="noboriptBox"><input id="email" name="email" class="noboript" type="text" value="<?php echo $loginInfo['email'] ?>">
                         </div>
                 </dd>
 
